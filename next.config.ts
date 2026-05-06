80b424e198c9a30329806fa54be41f03893e8d78
virat-mankali <viratmankali@gmail.com>
2026-05-06 17:16:53 +0530

    ✨ chore: set turbop root using fileURLTo to support ESM

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const projectRoot = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: path.resolve(__dirname),
+    root: projectRoot,
   },
 };
 
